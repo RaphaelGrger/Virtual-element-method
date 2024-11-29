28e0f91935cd187c08ee104ce6f7a8a003116770
Dylan-Cmt <cormet.dylan@gmail.com>
2024-11-29 20:09:53 +0100

    ajout code MATLAB de la docu

--- matlab/boundary_condition.m
+++
@@ -1,3 +0,0 @@
-function boundary_cond = boundary_condition(x, y)
-    boundary_cond = x * y * sin(pi * x);
-end
--- matlab/rhs.m
+++
@@ -1,3 +0,0 @@
-function rhs = rhs(x, y)
-    rhs = 15 * sin(pi * x) * sin(pi * y);
-end
