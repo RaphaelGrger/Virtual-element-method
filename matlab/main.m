690cf2c046a160c202a74b9daa66e520a31b2a30
Dylan-Cmt <cormet.dylan@gmail.com>
2024-12-11 21:53:30 +0100

    ajout des résultats dans res

--- matlab/main.m
+++ matlab/main.m
@@ -1,6 +1,17 @@
-input_path = 'C:\Users\corme\OneDrive\Bureau\Projet VEM\github\Virtual-element-method\meshes\'
-fname = 'L-domain.mat' % #'squares.mat', 'triangles.mat', 'voronoi.mat', 'smoothed-voronoi.mat', 'non-convex.mat', 'L-domain.mat'
+input_path = '..\meshes\';
+fname = 'triangles'; % 'squares', 'triangles', 'voronoi', 'smoothed-voronoi', 'non-convex', 'L-domain'
 
-mesh_filepath = fullfile(input_path,fname);
+% Chemin complet du fichier mesh
+mesh_filepath = fullfile(input_path, fname);
 
+% Appel de la fonction VEM
 vem(mesh_filepath, @square_domain_rhs, @square_domain_boundary_condition);
+
+% Enregistrement du plot
+output_dir = 'res'; % Dossier de sortie
+if ~exist(output_dir, 'dir')
+    mkdir(output_dir); % Crée le dossier "res" s'il n'existe pas
+end
+
+output_filepath = fullfile(output_dir, [fname, '.png']); % Crée le chemin complet avec l'extension
+saveas(gcf, output_filepath); % Enregistre le plot
