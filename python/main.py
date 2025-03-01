6777f5dd914ca6218f0ae92c44a4e7476c52a5d8
RaphaelGrger <raphael.06gr@gmail.com>
2025-03-01 18:13:59 +0100

    Update main.py

--- python/main.py
+++ python/main.py
@@ -7,31 +7,25 @@ import functions as fct
 import read_mesh as rm
 import vem
 
-################### Variables ###################
 
-fname = 'voronoi'
+fname =  'urban'
 input_path = '../meshes/'
     
 def test(x,y) :
     return x**2+y**2
 
+def boundary_test(coord):
+    return coord[:,0] + coord[:,1] 
+
 if __name__ == '__main__':
     
     Nelements, elements, vertices = rm.read_meshes(fname)
-    u,verts = vem.vem(fname,vem.rhs,vem.square_boundary_condition)
-    
+    u,verts, diam,K = vem.vem(fname,vem.rhs,vem.square_boundary_condition)
+
     V = rm.extract_meshes(elements,verts)
 
-    #middles,normals = read_mesh.mid_and_normals(meshes)
-    print("Somme des aires : {}".format(np.sum([fct.area(poly) for poly in V])))
-
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))         
     rm.draw_meshes(V,ax1)
-
-    #vem.plot_solution(vertices,test(vertices[:,0],vertices[:,1]),ax2)
-    
-    vem.plot_solution(vertices,u,ax2)
+            
+    vem.plot_solution(V,vertices,u,ax2)
     plt.show()
-
-
-    print(np.max(np.abs(vem.square_boundary_condition(vertices) - u)))
